56ef7ada31b8ea702080079d56223cdd1eb7d088
Caleb Doxsey <caleb@datadoghq.com>
2018-01-29 10:28:24 -0500

    add runner and generated files

---
+++ talks/2018-01-30--extending-gopherjs/generate.go
@@ -0,0 +1,9 @@
+package talk
+
+//go:generate gopherjs build -o assets/js/example-01.js ./example-01
+//go:generate gopherjs build -o assets/js/example-02.js ./example-02
+//go:generate gopherjs build -o assets/js/example-03.js ./example-03
+//go:generate gopherjs build -o assets/js/example-04.js ./example-04
+//go:generate gopherjs build -o assets/js/example-05.js ./example-05
+//go:generate gopherjs build -o assets/js/example-06.js ./example-06
+//go:generate gopherjs build -o assets/js/example-07.js ./example-07
