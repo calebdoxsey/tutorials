a886ac3bec33aab8657a9f8194670c8322bc415a
Caleb Doxsey <caleb@doxsey.net>
2019-08-11 22:23:14 -0600

    wip

--- talks/2019-08-15--tracing-with-go/01-example/book-worker/main.go
+++ talks/2019-08-15--tracing-with-go/01-example/book-worker/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"github.com/calebdoxsey/tutorials/talks/2019-08-15--tracing-with-go/01-example/pb"
 	"github.com/calebdoxsey/tutorials/talks/2019-08-15--tracing-with-go/01-example/pkg/deps"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/xerrors"
-	"net/url"
 )
 
 func main() {
@@ -16,6 +16,10 @@ func main() {
 	deps.RegisterTracer("book-worker")
 	c := deps.DialJobConsumer(ctx)
 
+	w := &worker{
+		db: deps.DialCockroach(ctx),
+	}
+
 	for {
 		jobs, err := c.Read(ctx)
 		if err != nil {
@@ -24,7 +28,7 @@ func main() {
 		for _, job := range jobs {
 			job := job
 			go func() {
-				err = handle(job)
+				err = w.handle(job)
 				if err != nil {
 					log.Error().Err(err).Msg("failed to process job")
 				}
@@ -37,37 +41,41 @@ func main() {
 	}
 }
 
-func handle(job *pb.Job) error {
+type worker struct {
+	db *sql.DB
+}
+
+func (w *worker) handle(job *pb.Job) error {
 	log.Info().Interface("job", job).Msg("processing job")
 	var err error
 	switch job.Type {
 	case pb.Job_DOWNLOAD:
-		err = download(job.Book)
+		err = w.download(job.Book)
 	case pb.Job_CALCULATE_STATS:
-		err = calculateStats(job.Book)
+		err = w.calculateStats(job.Book)
 	case pb.Job_GET_REVIEWS:
-		err = getReviews(job.Book)
+		err = w.getReviews(job.Book)
 	default:
 		err = xerrors.New("unknown job type")
 	}
+
+	status := "OK"
+	if err != nil {
+		status = err.Error()
+	}
+	if _, dberr := w.db.ExecContext(context.TODO(), `
+UPSERT INTO book_job_status (book_id, job_type, status) VALUES ($1, $2, $3)
+`, job.Book.Id, job.Type.String(), status); dberr != nil {
+		log.Warn().Err(dberr).Msg("failed to update job status")
+	}
+
 	return err
 }
 
-func download(book *pb.Book) error {
-	u, err := url.Parse(book.Url)
-	if err != nil {
-		return xerrors.Errorf("invalid book url: %w", err)
-	}
-
-	log.Info().Interface("url", u).Msg("received book download job")
-
+func (w *worker) calculateStats(book *pb.Book) error {
 	return xerrors.New("not implemented")
 }
 
-func calculateStats(book *pb.Book) error {
-	return xerrors.New("not implemented")
-}
-
-func getReviews(book *pb.Book) error {
+func (w *worker) getReviews(book *pb.Book) error {
 	return xerrors.New("not implemented")
 }
